12d2e41c42a3e7052b260f812ff12172e49c5b60
ilyosbekibrohimov <ilyos199822@gmail.com>
2021-07-07 15:52:37 +0900

    speech to text done

--- lib/business_logic/providers/search provider.dart
+++ lib/business_logic/providers/search provider.dart
@@ -1,13 +1,16 @@
 import 'dart:convert';
+import 'dart:math';
 
 import 'package:flutter/foundation.dart';
 import 'package:http/http.dart';
+import 'package:speech_to_text/speech_recognition_error.dart';
+import 'package:speech_to_text/speech_recognition_result.dart';
+import 'package:speech_to_text/speech_to_text.dart';
 import 'package:viewerapp/business_logic/services/web services.dart';
 import 'package:viewerapp/models/postslist_model.dart';
 import 'package:viewerapp/models/search%20model.dart';
 
 class SearchProvider extends ChangeNotifier {
-
   List<Post> _searchResults = [];
   List<SearchWord> _recentSearches = [];
   List<SearchWord> _relatedSearches = [];
@@ -33,10 +36,11 @@ class SearchProvider extends ChangeNotifier {
       return false;
     }
   }
+
   Future<bool> fetchRecentSearches(String memberId) async {
     bool result = false;
     if (_recentSearches.isNotEmpty) _recentSearches.clear();
-    try{
+    try {
       Response response = await WebServices.fetchRecentSearches(memberId);
       Map<String, dynamic> decodedResponse = json.decode(utf8.decode(response.bodyBytes));
       if (response.statusCode == 200) {
@@ -46,9 +50,8 @@ class SearchProvider extends ChangeNotifier {
         result = true;
         notifyListeners();
       }
-      return  result;
-    }
-    catch(e) {
+      return result;
+    } catch (e) {
       print(e);
       return false;
     }
@@ -81,8 +84,99 @@ class SearchProvider extends ChangeNotifier {
 
   List<SearchWord> get recentSearches => _recentSearches;
 
-  void  cleanList() {
-    if(_searchResults.isNotEmpty)
-    _searchResults.clear();
+  void cleanList() {
+    if (_searchResults.isNotEmpty) _searchResults.clear();
   }
+
+  //region Speech recognition
+  bool has_speech = false;
+  double level = 0.0;
+  double minSoundLevel = 50000;
+  double maxSoundLevel = -50000;
+  String lastWords = '';
+  String lastError = '';
+  String lastStatus = '';
+  String currentLocaleId = '';
+  int resultListened = 0;
+  String word1 = "";
+  bool word2 = false;
+  List<LocaleName> localeNames = [];
+  final SpeechToText speech = SpeechToText();
+
+  Future<void> initSpeechState() async {
+    var hasSpeech = await speech.initialize(
+      onError: errorListener,
+      onStatus: statusListener,
+      debugLogging: true,
+      finalTimeout: Duration(milliseconds: 0),
+    );
+    if (hasSpeech) {
+      localeNames = await speech.locales();
+      var systemLocale = await speech.systemLocale();
+      currentLocaleId = systemLocale?.localeId ?? '';
+    }
+    has_speech = hasSpeech;
+    notifyListeners();
+  }
+
+  void errorListener(SpeechRecognitionError error) {
+    lastError = error.errorMsg;
+    notifyListeners();
+  }
+
+  void statusListener(String status) {
+    lastStatus = status;
+    notifyListeners();
+  }
+
+  void startListening() {
+    lastWords = '';
+    lastError = '';
+    speech.listen(
+      onResult: resultListener,
+      listenFor: Duration(seconds: 30),
+      pauseFor: Duration(seconds: 5),
+      partialResults: true,
+      localeId: currentLocaleId,
+      onSoundLevelChange: soundLevelListener,
+      cancelOnError: true,
+      listenMode: ListenMode.confirmation
+    );
+    notifyListeners();
+  }
+
+  void stopListening() {
+    speech.stop();
+    level = 0.0;
+    notifyListeners();
+  }
+
+  void cancelListening() {
+    speech.cancel();
+    level = 0.0;
+    notifyListeners();
+  }
+
+  void resultListener(SpeechRecognitionResult result) {
+    ++resultListened;
+    print("Result listened $resultListened");
+    print("Results");
+    word1 = result.recognizedWords;
+    word2  = result.finalResult;
+    print(result.recognizedWords);
+    print(result.finalResult);
+    notifyListeners();
+  }
+
+  void soundLevelListener(double level) {
+    minSoundLevel = min(minSoundLevel, level);
+    maxSoundLevel = max(maxSoundLevel, level);
+    notifyListeners();
+  }
+
+  void switchLang(selectedVal) {
+    currentLocaleId = selectedVal;
+    notifyListeners();
+  }
+  //endregion
 }
--- lib/ui/helper_widgets/voice recorder modal bottom sheet.dart
+++ lib/ui/helper_widgets/voice recorder modal bottom sheet.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:viewerapp/business_logic/providers/search%20provider.dart';
 
 class VoiceRecorderModalBottomSheet extends StatelessWidget {
   double height;
@@ -7,41 +9,79 @@ class VoiceRecorderModalBottomSheet extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: height,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Container(
-            alignment: Alignment.bottomRight,
-            child: IconButton(
-                icon: Icon(
-                  Icons.clear,
-                  size: 30,
-                ),
-                onPressed: () {
-                  Navigator.pop(context);
-                }),
-          ),
-          Container(
-            margin: EdgeInsets.only(top: 30),
-            child: Text(
-              "듣고 있습니다...말씀하세요 🙂",
-              style: TextStyle(fontSize: 20),
-            ),
-          ),
-          Container(
-              decoration: BoxDecoration(color: Colors.red, border: Border.all(color: Colors.red), borderRadius: BorderRadius.all(Radius.circular(30))),
-              margin: EdgeInsets.only(bottom: 30),
+    return Consumer<SearchProvider>(builder: (context, search, child) {
+      return Container(
+        height: height,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Container(
+              alignment: Alignment.bottomRight,
               child: IconButton(
                   icon: Icon(
-                    Icons.mic_sharp,
-                    color: Colors.white,
+                    Icons.clear,
                     size: 30,
                   ),
-                  onPressed: () {}))
-        ],
-      ),
-    );
+                  onPressed: () {
+                    Navigator.pop(context);
+                  }),
+            ),
+            Container(
+              child: Text("Recognized words:  ${search.word1}"),),
+            Container(
+              margin: EdgeInsets.only(top: 30),
+              child: Text(
+                "듣고 있습니다...말씀하세요 🙂",
+                style: TextStyle(fontSize: 20),
+              ),
+            ),
+            Container(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  TextButton(
+                      onPressed: () {
+                        if (search.has_speech || !search.speech.isListening) search.startListening();
+                      },
+                      child: Text("Start")),
+                  TextButton(
+                      onPressed: () {
+                        if (search.speech.isListening) search.stopListening();
+                      },
+                      child: Text("Stop")),
+                  TextButton(
+                      onPressed: () {
+                        if (search.speech.isListening) search.cancelListening();
+                      },
+                      child: Text("Cancel")),
+                ],
+              ),
+            ),
+            Container(
+              child: Row(
+              children: [
+                DropdownButton(
+                    onChanged: (selectedValue) => search.switchLang(selectedValue),
+                    value: search.currentLocaleId,
+                    items: search.localeNames.map((e) => DropdownMenuItem(child: Text(e.name),  value: e.localeId,)).toList())
+              ],
+            ),),
+
+            Container(
+                decoration: BoxDecoration(color: Colors.red, border: Border.all(color: Colors.red), borderRadius: BorderRadius.all(Radius.circular(30))),
+                margin: EdgeInsets.only(bottom: 30),
+                child: IconButton(
+                    icon: Icon(
+                      Icons.mic_sharp,
+                      color: Colors.white,
+                      size: 30,
+                    ),
+                    onPressed: () async {
+                      if (!search.has_speech) await search.initSpeechState();
+                    }))
+          ],
+        ),
+      );
+    });
   }
 }
